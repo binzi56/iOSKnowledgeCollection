c9b467b795698e2a496f56a2e1cea3386856b2ff
火之玉 <1533607721@qq.com>
2019-02-13 09:50:50 +0800

    提交

---
+++ Multithreading/Multithreading/Demo2/KCPerson.h
@@ -0,0 +1,17 @@
+//
+//  KCPerson.h
+//  Multithreading
+//
+//  Created by 帅斌 on 2019/2/13.
+//  Copyright © 2019年 dongming. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface KCPerson : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Multithreading/Multithreading/Demo2/KCPerson.m
@@ -0,0 +1,13 @@
+//
+//  KCPerson.m
+//  Multithreading
+//
+//  Created by 帅斌 on 2019/2/13.
+//  Copyright © 2019年 dongming. All rights reserved.
+//
+
+#import "KCPerson.h"
+
+@implementation KCPerson
+
+@end
